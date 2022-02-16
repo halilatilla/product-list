370e2912b7df2ff5385e6851f50bd904219ba336
halilatilla <atillahalil0@gmail.com>
2022-02-16 18:07:55 +0300

    Add Header comp.

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,4 @@
 export { default as Search } from './Form/Search/Search'
 export { default as Button } from './Button/Button'
+export { default as Header } from './Header/Header'
+export { default as Logo } from './Logo/Logo'
